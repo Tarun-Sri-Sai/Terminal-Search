08f0a29c56a41a4bcf4d846114a6cbb216746d53
Tarun <tarunsrisaiv03@gmail.com>
2023-06-29 08:09:29 +0530

    Remove bin_exec

--- src/search.c
+++ src/search.c
@@ -6,27 +6,27 @@
 #define STRING_LIMIT (0xFFFF / sizeof(char))
 #define IS_EQUAL 0
 
-void display_help(const char *bin_exec);
-void display_args_limit(const char *bin_exec);
+void display_help();
+void display_args_limit();
 bool is_switch(const char *arg);
-int exec_switch(const char *switch_sub_cmd, const char *bin_exec);
+int exec_switch(const char *switch_sub_cmd);
 void display_wrong_switch();
 
 int main(const int argc, char **argv)
 {
     if (argc == 1)
     {
-        display_help(argv[0]);
+        display_help();
         return EXIT_FAILURE;
     }
     if (argc > 2)
     {
-        display_args_limit(argv[0]);
+        display_args_limit();
         return EXIT_FAILURE;
     }
     if (is_switch(argv[1]))
     {
-        return exec_switch(argv[1], argv[0]);
+        return exec_switch(argv[1]);
     }
     char *search_query = (char *)calloc(STRING_LIMIT, sizeof(char)), *token;
 
@@ -47,15 +47,15 @@ int main(const int argc, char **argv)
     return EXIT_SUCCESS;
 }
 
-void display_help(const char *bin_exec)
+void display_help()
 {
-    printf("Command usage: %s \"search query\" or --help to show this.\n", bin_exec);
+    printf("Command usage: search \"search query\" or --help to show this.\n");
 }
 
-void display_args_limit(const char *bin_exec)
+void display_args_limit()
 {
     printf("Enclose the query in quotation marks if there are more than one query words.\n");
-    display_help(bin_exec);
+    display_help();
 }
 
 bool is_switch(const char *arg)
@@ -66,11 +66,11 @@ bool is_switch(const char *arg)
             || (length >= 1 && arg[0] == '-'));
 }
 
-int exec_switch(const char *switch_sub_cmd, const char *bin_exec)
+int exec_switch(const char *switch_sub_cmd)
 {
     if (strcmp(switch_sub_cmd, "--help") == IS_EQUAL)
     {
-        display_help(bin_exec);
+        display_help();
         return EXIT_SUCCESS;
     }
     display_wrong_switch();
